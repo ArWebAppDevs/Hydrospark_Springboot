6042e995b78b6e6f132d4f25802c1496161fbcaf
ArWebAppDevs <ar.webappdevs@gmail.com>
2025-04-05 02:52:42 +0530

    Hydrospark

--- src/main/java/com/hydrospark/hydrospark/configuration/GlobalAspect.java
+++ src/main/java/com/hydrospark/hydrospark/configuration/GlobalAspect.java
@@ -34,20 +34,28 @@ public class GlobalAspect {
             Hydrospark hyd = hydrosparkRepo.findByName("Hydro1...").get(0);
             Hydrospark Banner1 = hydrosparkRepo.findByName("Banner1").get(0);
             Hydrospark Banner2 = hydrosparkRepo.findByName("Banner2").get(0);
+            Hydrospark Banner3 = null;
+            if (hydrosparkRepo.findByName("Banner3").size() > 0) {
+                Banner3 = hydrosparkRepo.findByName("Banner3").get(0);
+            }
             Blob blob = new SerialBlob(hyd.getImg());
             Blob Banner1Blob = new SerialBlob(Banner1.getImg());
             Blob Banner2Blob = new SerialBlob(Banner2.getImg());
+            Blob Banner3Blob = new SerialBlob(Banner3.getImg());
             byte[] bytes = blob.getBytes(1, (int) blob.length());
             byte[] Banner1bytes = Banner1Blob.getBytes(1, (int) Banner1Blob.length());
             byte[] Banner2bytes = Banner2Blob.getBytes(1, (int) Banner2Blob.length());
+            byte[] Banner3bytes = Banner3Blob.getBytes(1, (int) Banner3Blob.length());
             String base64Image = Base64.getEncoder().encodeToString(bytes);
             String Banner1base64Image = Base64.getEncoder().encodeToString(Banner1bytes);
             String Banner2base64Image = Base64.getEncoder().encodeToString(Banner2bytes);
+            String Banner3base64Image = Base64.getEncoder().encodeToString(Banner3bytes);
 
             // Storing the Base64 image string in the session
             session.setAttribute("img", base64Image);
-            session.setAttribute("Banner1", Banner1base64Image);
-            session.setAttribute("Banner2", Banner2base64Image);
+            session.setAttribute("banner1", Banner1base64Image);
+            session.setAttribute("banner2", Banner2base64Image);
+            session.setAttribute("banner3", Banner2base64Image);
         }
     }
 }
